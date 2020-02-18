cbd9c98a105c02dbf637dda7e4c23fed11ebfddb
Nikkolai <nikkolaifernandez14@gmail.com>
2020-02-18 23:36:13 +0800

    Update site

---
+++ index.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta content="width=device-width, initial-scale=1" name="viewport"/>
+        <title>
+            Nikkolai Fernandez | Full-Stack PHP Developer
+        </title>
+        <link href="assets/css/main.css" rel="stylesheet" type="text/css">
+    </head>
+    <body class="container">
+        <main id="home">
+            <section class="self-img-wrapper">
+                <img class="self-img" src="assets/img/self.jpeg">
+                </img>
+            </section>
+            <section class="message-wrapper">
+                <h1 class="message-title">
+                    Hello, I'm Nikkolai Fernandez
+                </h1>
+                <p class="message-headline">
+                    A full-stack PHP developer
+                </p>
+            </section>
+            <ul class="list-items-v-wrapper">
+                <li class="list-items-v-item footer-item">
+                    <a href="https://www.facebook.com/iamnikkolai" target="_blank" title="Facebook">
+                        <img class="footer-icon" src="assets/img/facebook-logo.png">
+                        </img>
+                    </a>
+                </li>
+                <li class="list-items-v-item footer-item">
+                    <a href="https://twitter.com/IAmNikkolai14" target="_blank" title="Twitter">
+                        <img class="footer-icon" src="assets/img/twitter-logo.png">
+                        </img>
+                    </a>
+                </li>
+                <li class="list-items-v-item footer-item">
+                    <a href="https://github.com/nikkolai14" target="_blank" title="Github">
+                        <img class="footer-icon" src="assets/img/github-logo.png">
+                        </img>
+                    </a>
+                </li>
+                <li class="list-items-v-item footer-item">
+                    <a href="https://www.linkedin.com/in/iamnikkolai/" target="_blank" title="LinkedkIn">
+                        <img class="footer-icon" src="assets/img/linkedin-logo.png">
+                        </img>
+                    </a>
+                </li>
+                <li class="list-items-v-item footer-item">
+                    <a href="https://stackoverflow.com/users/7574968/nikkolai-fernandez" target="_blank" title="Stack Overflow">
+                        <img class="footer-icon" src="https://cdn.sstatic.net/Sites/stackoverflow/company/img/logos/so/so-icon.svg?v=f13ebeedfa9e">
+                        </img>
+                    </a>
+                </li>
+                <li class="list-items-v-item footer-item">
+                    <a href="https://www.hackerrank.com/nikkolai" target="_blank" title="HackerRank">
+                        <img class="footer-icon" src="assets/img/hackerRank-logo.png">
+                        </img>
+                    </a>
+                </li>
+            </ul>
+            <p class="text-center">
+                I’m a professional Web Developer with a focus on PHP and Laravel.
+                <br>
+                I’m an avid practitioner of best practices and Agile development.
+                <br>
+                I enjoy reading technical books and improving my knowledge.
+            </p>
+        </main>
+
+        <h2 class="text-center" id="projects">
+            Projects
+        </h2>
+        <ul>
+	        <li>
+	            <a class="link" href="http://abukai.com/?id=crm" target="_blank">
+	                Customer Relationship Management
+	            </a>
+	            - It includes a common features that a CRM should have then added some specific ones.
+	        </li>
+	        <li>
+	            <a class="link" href="https://ehr.themobiledrs.com/" target="_blank">
+	                Electronic Health Record
+	            </a>
+	            - A medical office system where staff can manage patient infos, visits, physican tasks and more.
+	        </li>
+	        <li>
+	            <a class="link" href="https://www.salonbossmarketing.com/" target="_blank">
+	                Website Builder
+	            </a>
+	            - A simplified CMS targeted for salon professionals.
+	        </li>
+	        <li>
+                <a class="link" href="https://www.facebook.com/agrifundfinance" target="_blank">
+                    Loans Management System
+                </a>
+	            - Branches use this system to track its own branch records, headoffice staffs able to manage loans <br> payments of customers and generation of reports.
+	        </li>
+	        <li>
+	            <a class="link" href="https://github.com/hpez/PHPBignum" target="_blank">
+	                PHPBignum
+	            </a>
+	            - This is a Bignum math library where I contributed to the usage documentation & Bigfloat power function.
+	        </li>
+	    </ul>
+
+        <h2 class="text-center" id="skills">
+            Skills
+        </h2>
+        <ul>
+            <li>
+                <strong>
+                    Front-end -
+                </strong>
+                HTML, CSS, SASS, Javascript, Jquery, Bootstrap, Foundation by Zurb
+            </li>
+            <li>
+                <strong>
+                    Back-end -
+                </strong>
+                PHP, MySQL, SQL, Laravel, CodeIgniter, Symfony, Twig, PHPUnit, Wordpress
+            </li>
+            <li>
+                <strong>
+                    SDK/API -
+                </strong>
+                PayPal, Stripe, Nexmo SMS, Dropbox
+            </li>
+            <li>
+                <strong>
+                    Servers -
+                </strong>
+                Ubuntu, Linode, AWS EC2, Apache HTTP
+            </li>
+            <li>
+                <strong>
+                    Practices -
+                </strong>
+                Unit Testing, Relational DB Design, MVC, GitFlow
+            </li>
+            <li>
+                <strong>
+                    Security -
+                </strong>
+                XSS,SQLi, IDOR, CSRF
+            </li>
+            <li>
+                <strong>
+                    Tools -
+                </strong>
+                Photoshop, FileZilla, Sublime Text, Git, XAMPP, MySQL Workbench
+            </li>
+        </ul>
+
+        <h2 class="text-center" id="contact">
+            Contact
+        </h2>
+        <p class="text-center">
+            Want to chat? You can get in touch with me on any of my social media profiles or through a good old-fashioned
+            <a class="link" href="mailto:nikkolaifernandez14@gmail.com">
+                email
+            </a>
+            .
+        </p>
+    </body>
+</html>
